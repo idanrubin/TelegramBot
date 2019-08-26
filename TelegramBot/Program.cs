767766b1c3ab1928717348fd5d6bbb4a67e01e8d
Idan Rubin <idanr@nayax.com>
2019-08-26 10:19:19 +0300

    add key

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -7,7 +7,7 @@ namespace TelegramBot
 {
     class Program
     {
-     private static readonly  TelegramBotClient Bot = new TelegramBotClient("u");
+     private static readonly  TelegramBotClient Bot = new TelegramBotClient("839932235:AAGKWtiKBP1pn4IWxG9Wqvv-fOF0HLAUi6Y");
 
         static void Main(string[] args)
         {
